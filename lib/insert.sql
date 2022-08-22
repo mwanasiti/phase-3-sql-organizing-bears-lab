e512c3daf6560c7ce12d63adf82e729267a02b99
Mwanasiti <ali.mariam.mwanasiti@gmail.com>
2022-08-22 09:21:02 +0300

    commit tree 18165c8810ef62f70a15b27d92bb0eb7037d0187

---
+++ lib/insert.sql
@@ -0,0 +1,23 @@
+INSERT INTO bears (name, age, sex, color, temperament, alive)
+VALUES ('Mr. Chocolate', 7, 'M', 'Black', '70', 1);
+
+INSERT INTO bears (name, age, sex, color, temperament, alive)
+VALUES ('Rowdy', 3, 'M', 'Brown', '90', 1);
+
+INSERT INTO bears (name, age, sex, color, temperament, alive)
+VALUES ('Tabitha', 10, 'F', 'Black', '40', 1);
+
+INSERT INTO bears (name, age, sex, color, temperament, alive)
+VALUES ('Sergeant Brown', 11, 'M', 'Brown', '60', 0);
+
+INSERT INTO bears (name, age, sex, color, temperament, alive)
+VALUES ('Melissa', 5, 'F', 'Black', '50', 1);
+
+INSERT INTO bears (name, age, sex, color, temperament, alive)
+VALUES ('Grinch', 12, 'M', 'Green', '100', 1);
+
+INSERT INTO bears (name, age, sex, color, temperament, alive)
+VALUES ('Wendy', 7, 'F', 'Brown', '40', 1);
+
+INSERT INTO bears (name, age, sex, color, temperament, alive)
+VALUES (NULL, 1, 'M', 'Brown', '10', 1);
